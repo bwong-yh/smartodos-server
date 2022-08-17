2ca9958838594741811be409739d080704a3065c
Billy Wong <bwong.yh@protonmail.com>
2022-08-17 10:51:41 -0600

    complete todo put & delete req

--- server.js
+++ server.js
@@ -19,12 +19,12 @@ app.post('/todos', async (req, res) => {
     const { name } = req.body;
     const created_at = new Date();
 
-    const newTodo = await pool.query(
+    await pool.query(
       "INSERT INTO todos (name, created_at) VALUES ($1, $2) RETURNING *",
       [name, created_at]
     );
 
-    res.send(newTodo.rows);
+    res.json("Todo Created.");
   } catch (err) {
     console.log(err.messsage);
   }
@@ -34,14 +34,51 @@ app.post('/todos', async (req, res) => {
 app.get('/todos', async (req, res) => {
   try {
     const allTodos = await pool.query("SELECT * FROM todos;");
-    res.send(allTodos.rows);
+
+    res.json(allTodos.rows);
   } catch (err) {
     console.log(err.message);
   }
 });
 
 // get a todo
+app.get('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const todo = await pool.query("SELECT * FROM todos WHERE id = $1;", [id]);
+
+    res.json(todo.rows);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
 
 // update a todo
+app.put('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name } = req.body;
+
+    await pool.query(
+      "UPDATE todos SET name = $1 WHERE id = $2 RETURNING *;",
+      [name, id]);
+
+    res.json("Todo Updated.");
+  } catch (err) {
+    console.log(err.message);
+  }
+});
 
 // delete a todo
+app.delete('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    await pool.query("DELETE FROM todos WHERE id = $1", [id]);
+
+    res.json("Todo Deleted.");
+  } catch (err) {
+    console.log(err.message);
+  }
+});
